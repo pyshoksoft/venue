df2219793bfb7a460a5f759f1e996ccc652a8c26
Yurii Galushka <yurii.galushka@gmail.com>
2018-09-08 22:06:56 +0300

    Carrousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './styles.css';
 import Header from './components/Header';
+import Featured from './components/featured';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App" style={{height: '1200px', background: 'cornflowerblue'}}>
 				<Header />
+				<Featured />
 			</div>	
 		);
 	}
